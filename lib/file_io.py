39a64dab03b31b979681e3896b64030f112779db
Liban Golo <liban.golo@student.moringaschool.com>
2025-11-28 11:08:35 +0300

    python lab done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
+
+
 def write_file(file_name, file_content):
-    pass
+    """Writes content to a .txt file, overwriting if it exists."""
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content) 
+
 
 def append_file(file_name, append_content):
-    pass
+    """Appends content to a .txt file."""
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)  
+
 
 def read_file(file_name):
-    pass
+    """Reads the content of a .txt file and returns it as a string."""
+    with open(f"{file_name}.txt", "r") as f:
+        return f.read()
